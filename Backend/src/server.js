c370d13e4aca19e50641aba18ddf75657fd9fe83
kavita yadav <151850926+Kavita1928@users.noreply.github.com>
2025-12-28 23:51:01 +0530

    ports made
    login signup and logout ports made

---
+++ Backend/src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/auth/signup", (req, res) => {
+  res.send("Signup endpoint");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  res.send("Login endpoint");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  res.send("Logout endpoint");
+});
+
+app.listen(3000, () => console.log("server running on port 3000"));
